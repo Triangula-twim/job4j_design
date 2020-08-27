bf722122ead2709bd883e25fde659243bf4974c1
master <yuriy_frolov_2019@mail.ru>
2020-08-27 20:43:31 +0300

    4. Переопределить только equals [#346948]

--- chapter_001/src/main/java/ru/job4j/collection/User.java
+++ chapter_001/src/main/java/ru/job4j/collection/User.java
@@ -13,11 +13,6 @@ public class User {
         this.birthday = birthday;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
-    }
-
     public static void main(String[] args) {
         User first = new User("Ivan", 1, new GregorianCalendar(1993, Calendar.APRIL, 24));
         User second = new User("Ivan", 1, new GregorianCalendar(1993, Calendar.APRIL, 24));
@@ -26,5 +21,16 @@ public class User {
         map.put(second, "second");
         System.out.println(map);
         map.forEach((user, o) -> System.out.println(user.hashCode()));
+        System.out.println(first.equals(second));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children &&
+                Objects.equals(name, user.name) &&
+                Objects.equals(birthday, user.birthday);
     }
 }
