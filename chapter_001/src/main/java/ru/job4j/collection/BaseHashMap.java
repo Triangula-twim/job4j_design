e6a5552afb5253f77805f72339b9a5f71e13c52b
master <yuriy_frolov_2019@mail.ru>
2020-09-02 03:42:16 +0300

    8. Реализовать собственную структуру данных - HashMap [#346952]

---
+++ chapter_001/src/main/java/ru/job4j/collection/BaseHashMap.java
@@ -0,0 +1,115 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class BaseHashMap<K, V> implements Iterable<V> {
+
+    private Node<K, V>[] array;
+    private float threshold;
+    private int size = 0;
+
+    public BaseHashMap() {
+        this.array = new Node[16];
+        this.threshold = array.length * 0.75f;
+    }
+
+    public boolean insert(K key, V value) {
+        if (size + 1 >= threshold) {
+            threshold *= 2;
+            arrayDoubling();
+        }
+        Node<K, V> newNode = new Node<>(key, value);
+        int index = hash(key);
+        if (array[index] == null) {
+            array[index] = newNode;
+            size++;
+            return true;
+        }
+        return false;
+    }
+
+    private void arrayDoubling() {
+        Node<K, V>[] oldArray = array;
+        array = new Node[oldArray.length * 2];
+        size = 0;
+        for (Node<K, V> el:
+             oldArray) {
+            if (el != null) {
+                insert(el.getKey(), el.getValue());
+            }
+        }
+    }
+
+    public V get(K key) {
+        int index = hash(key);
+        if (array[index] == null) {
+            return null;
+        }
+        return array[index].getValue();
+    }
+
+    public boolean delete(K key) {
+        int index = hash(key);
+        if (array[index] == null) {
+            return false;
+        }
+        array[index] = null;
+        size--;
+        return true;
+    }
+
+    private int hash(K key) {
+        return key.hashCode() % array.length;
+    }
+
+    @Override
+    public Iterator<V> iterator() {
+        return new Itr();
+    }
+
+    private class Itr implements Iterator<V>{
+        private int countKey = 0;
+        private int counter = 0;
+
+        @Override
+        public boolean hasNext() {
+            return countKey != size;
+        }
+
+        @Override
+        public V next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            for (int i = countKey; i < array.length; i++) {
+                if (array[i] != null) {
+                    countKey = i;
+                }
+            }
+            return array[countKey].getValue();
+        }
+    }
+
+    private class Node<K, V> {
+        private K key;
+        private V value;
+
+        public Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/collection/BaseHashMapTest.java
@@ -0,0 +1,78 @@
+package ru.job4j.collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+
+public class BaseHashMapTest {
+
+    @Test
+    public void getOne() {
+        BaseHashMap map = new BaseHashMap();
+        map.insert(1, "one");
+        map.insert(2, "two");
+        Assert.assertEquals(map.get(2), "two");
+    }
+
+    @Test
+    public void insertOne() {
+        BaseHashMap map = new BaseHashMap();
+        Assert.assertTrue(map.insert(1, "one"));
+    }
+
+    @Test
+    public void deleteOne() {
+        BaseHashMap map = new BaseHashMap();
+        map.insert(1, "one");
+        map.insert(2, "two");
+        map.delete(2);
+        Assert.assertNull(map.get(2));
+    }
+
+    @Test
+    public void deleteTwo() {
+        BaseHashMap map = new BaseHashMap();
+        map.insert(1, "one");
+        map.insert(2, "two");
+        map.delete(1);
+        Assert.assertNull(map.get(1));
+    }
+
+    @Test
+    public void iteratorHasNextTrue() {
+        BaseHashMap map = new BaseHashMap();
+        map.insert(1, "one");
+        Iterator<Integer> iterator = map.iterator();
+        Assert.assertTrue(iterator.hasNext());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void iteratorDeleteNext() {
+        BaseHashMap map = new BaseHashMap();
+        map.insert(1, "one");
+        map.delete(1);
+        Iterator<Integer> iterator = map.iterator();
+        iterator.next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void iteratorNextNext() {
+        BaseHashMap map = new BaseHashMap();
+        map.insert(1, "one");
+        Iterator<Integer> iterator = map.iterator();
+        iterator.next();
+        iterator.next();
+    }
+
+    @Test
+    public void iteratorNext() {
+        BaseHashMap map = new BaseHashMap();
+        map.insert(1, "one");
+        Iterator<Integer> iterator = map.iterator();
+        iterator.next();
+    }
+}
