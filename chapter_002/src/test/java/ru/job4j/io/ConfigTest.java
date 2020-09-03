ff297024df77b5e4853c665b99570e2d413c473c
master <yuriy_frolov_2019@mail.ru>
2020-09-03 13:15:43 +0300

    1. Читаем файл конфигурации [#346971]

---
+++ chapter_002/src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutCommentName() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("name"),
+                is("Yuriy")
+        );
+    }
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "./data/pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(
+                config.value("surname"),
+                is("Frolov")
+        );
+    }
+}
