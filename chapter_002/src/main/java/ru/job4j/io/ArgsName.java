83cb49508b7f153f916e970e1659f4aed7721fc3
master <yuriy_frolov_2019@mail.ru>
2020-09-04 16:19:03 +0300

    5.1. Именованные аргументы [#346969]

---
+++ chapter_002/src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,41 @@
+package ru.job4j.io;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException(
+                    "Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+        }
+        Arrays.stream(args)
+                .filter(s -> !s.equals(""))
+                .filter(s -> s.contains("="))
+                .map(s -> s.split("="))
+                .filter(strings -> strings.length == 2)
+                .forEach(strings -> this.values.put(strings[0].replace("-", ""), strings[1]));
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
